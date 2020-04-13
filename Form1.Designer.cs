b6151df03cf2a2fe9f368a1a64d8543307e29644
Bruno <Bruno@BRUNO-PC-N>
2020-04-13 15:27:21 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace CabInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxOpen = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CAB Installer";
+            // 
+            // tbxOpen
+            // 
+            this.tbxOpen.Location = new System.Drawing.Point(12, 54);
+            this.tbxOpen.Name = "tbxOpen";
+            this.tbxOpen.Size = new System.Drawing.Size(185, 20);
+            this.tbxOpen.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(203, 53);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(41, 21);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 80);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(232, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 120);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.tbxOpen);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CAB Installer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxOpen;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CabInstaller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tbxOpen.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Title = "Ouvrir fichier CAB";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+
+            btnStart.Text = "Installation in progress....";
+
+            bool exists = System.IO.Directory.Exists(@"C:\Temp_CAB");
+
+            if (!exists)
+            {
+                System.IO.Directory.CreateDirectory(@"C:\Temp_CAB");
+            }
+
+            bool exists2 = System.IO.Directory.Exists(@"C:\Temp_CAB\INF_FILES");
+
+            if (!exists2)
+            {
+                System.IO.Directory.CreateDirectory(@"C:\Temp_CAB\INF_FILES");
+            }
+
+            Process proc = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "/k echo F|xcopy /S /Q /Y /F \"" + tbxOpen.Text + "\" C:\\Temp_CAB\\DRIVERS_TO_INSTALL.cab* & exit";
+            proc.StartInfo.ErrorDialog = true;
+            proc.StartInfo.Verb = "runas";
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.Start();
+            proc.WaitForExit();
+
+            Process proc2 = new Process();
+            proc2.StartInfo.FileName = "cmd.exe";
+            proc2.StartInfo.Arguments = "/k expand C:\\Temp_CAB\\DRIVERS_TO_INSTALL.cab -F:* C:\\Temp_CAB\\INF_FILES & exit";
+            proc2.StartInfo.ErrorDialog = true;
+            proc2.StartInfo.Verb = "runas";
+            proc2.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc2.Start();
+            proc2.WaitForExit();
+
+            Process proc3 = new Process();
+            proc3.StartInfo.FileName = "cmd.exe";
+            proc3.StartInfo.WorkingDirectory = "C:\\Temp_CAB\\INF_FILES";
+            proc3.StartInfo.Arguments = "/k for /f %i in ('dir /b /s *.inf') do pnputil.exe -i -a %i & exit";
+            proc3.StartInfo.ErrorDialog = true;
+            proc3.StartInfo.Verb = "runas";
+            proc3.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc3.Start();
+            proc3.WaitForExit();
+
+            btnStart.Text = "Installation finished";
+
+            Cursor.Current = Cursors.Default;
+        }
+    }
+}
